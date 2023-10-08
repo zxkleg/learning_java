04ea3452b958a793007b0334b17600b65b5e3ba0
zxkleg <kytseval.y.r@mail.ru>
2023-10-08 20:23:36 +0500

    feat: added food command

--- src/main/java/org/zxkleg/myplugin/Plugin.java
+++ src/main/java/org/zxkleg/myplugin/Plugin.java
@@ -4,6 +4,7 @@ package org.zxkleg.myplugin;
 import co.aikar.commands.BukkitCommandManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.zxkleg.myplugin.commands.FavouriteFood;
 import org.zxkleg.myplugin.commands.SayHello;
 import org.zxkleg.myplugin.events.Events;
 
@@ -11,16 +12,23 @@ public final class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Plugin is working!!");
+        getLogger().info("Plugin is setting!!");
+
+        saveDefaultConfig();
 
         Bukkit.getPluginManager().registerEvents(new Events(), this);
 
         BukkitCommandManager manager = new BukkitCommandManager(this);
         manager.registerCommand(new SayHello());
+        manager.registerCommand(new FavouriteFood(this));
+
+        getLogger().info("Plugin is working!!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Plugin has turned off!!");
     }
+
+
 }
---
+++ src/main/java/org/zxkleg/myplugin/commands/FavouriteFood.java
@@ -0,0 +1,38 @@
+package org.zxkleg.myplugin.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.zxkleg.myplugin.Plugin;
+
+import java.util.Arrays;
+
+@CommandAlias("food")
+public class FavouriteFood extends BaseCommand {
+
+    private final Plugin plugin;
+
+    public FavouriteFood(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Default
+    public void onFavouriteFood(CommandSender sender, String[] args) {
+        if (sender instanceof Player player) {
+            if (args.length > 0) {
+                String newFavouriteFood = String.join("", args);
+                this.plugin.getConfig().set("favourite-food", newFavouriteFood);
+                this.plugin.saveConfig();
+                player.sendMessage("Любимая еда была изменена на " + newFavouriteFood);
+
+            } else {
+                String favouriteFood = this.plugin.getConfig().getString("favourite-food");
+                player.sendMessage("Любимая еда - " + favouriteFood);
+            }
+        } else {
+            sender.sendMessage("Команда может быть выполнена только оператором, не консолью");
+        }
+    }
+}
