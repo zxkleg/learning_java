b7af558b7aaf95614d60bb715651c1a628c8aa49
zxkleg <kytseval.y.r@mail.ru>
2023-10-08 12:14:07 +0500

    feat: hello command

--- src/main/java/org/zxkleg/myplugin/Plugin.java
+++ src/main/java/org/zxkleg/myplugin/Plugin.java
@@ -1,7 +1,10 @@
 package org.zxkleg.myplugin;
 
+
+import co.aikar.commands.BukkitCommandManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.zxkleg.myplugin.commands.SayHello;
 import org.zxkleg.myplugin.events.Events;
 
 public final class Plugin extends JavaPlugin {
@@ -11,6 +14,9 @@ public final class Plugin extends JavaPlugin {
         getLogger().info("Plugin is working!!");
 
         Bukkit.getPluginManager().registerEvents(new Events(), this);
+
+        BukkitCommandManager manager = new BukkitCommandManager(this);
+        manager.registerCommand(new SayHello());
     }
 
     @Override
---
+++ src/main/java/org/zxkleg/myplugin/commands/SayHello.java
@@ -0,0 +1,21 @@
+package org.zxkleg.myplugin.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandAlias("hello")
+public class SayHello extends BaseCommand {
+    @Default
+    @CommandPermission("minecraft.command.op")
+    public void onSayHello(CommandSender sender) {
+        if (sender instanceof Player player) {
+            player.sendMessage("Привет, " + player.getName() + "!");
+        } else {
+            sender.sendMessage("Команда может быть выполнена только оператором");
+        }
+    }
+}
